dd8abf709ccc8e0f73f1345ed7a536f09a1714d3
Mr-SpaceMonkey <dario.potenza02@gmail.com>
2017-11-11 15:09:11 +0000

    Made smooth camera movement
    The camera is a lot smoother but needs a lot of work

---
+++ Skating game/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+    public Transform target;
+
+    public float smoothSpeed;
+    public float distanceAway;
+    public float distanceUp;
+    Vector3 targetPosition;
+
+    void FixedUpdate()
+    {
+        targetPosition = target.position + target.up * distanceUp - target.forward * distanceAway;
+        Debug.DrawRay(target.position, Vector3.up * distanceUp, Color.red);
+        Debug.DrawRay(target.position, -1f * target.forward * distanceAway,Color.blue);
+        Debug.DrawLine(target.position, targetPosition, Color.magenta);
+        transform.position = Vector3.Lerp(transform.position, targetPosition, Time.deltaTime * smoothSpeed);
+        transform.LookAt(target);
+    }
+
+}
--- Skating game/Assets/Scripts/GameManager.cs
+++ Skating game/Assets/Scripts/GameManager.cs
@@ -3,22 +3,9 @@ using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour {
 
-    public GameObject music;
-
-    private void Awake()
-    {
-        if (music == null)
-        {
-            Instantiate(music);
-        }
-
-    }
-
     public void Respawn()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
-        DontDestroyOnLoad(music);
-
     }
 
 }
