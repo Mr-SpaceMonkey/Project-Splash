1e730f39e841dc8807fc8da016df2c1f6d1be48b
Mr-SpaceMonkey <dario.potenza02@gmail.com>
2017-11-11 23:36:07 +0000

    Ground Movement
    Made deck model FBX, made the board symetrical to Player GameObject, Changed type of movement to using wheel colliders

---
+++ Skating game/Assets/Scripts/CentreOfMass.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class CentreOfMass : MonoBehaviour {
+
+    public Vector3 cOM;
+    Rigidbody rb;
+
+	// Use this for initialization
+	void Start ()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.centerOfMass = cOM;
+	}
+	
+}
---
+++ Skating game/Assets/Scripts/PlayerMotor.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class PlayerMotor : MonoBehaviour {
+
+    [Header("Initialising Wheel Colliders")]
+    public WheelCollider frontLeft;
+    public WheelCollider frontRight;
+    public WheelCollider backLeft;
+    public WheelCollider backRight;
+
+    public float motorForce;
+    public float steerForce;
+
+    // Update is called once per frame
+    void Update ()
+    {
+        float motor = Input.GetAxis("Fire1") * motorForce;
+        float turn = Input.GetAxis("Horizontal") * steerForce;
+
+        backLeft.motorTorque = motor;
+        backRight.motorTorque = motor;
+        frontLeft.motorTorque = motor;
+        frontRight.motorTorque = motor;
+
+        frontLeft.steerAngle = turn;
+        frontRight.steerAngle = turn;
+	}
+}
