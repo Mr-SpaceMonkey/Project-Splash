6ba2ff66da67dfa346c91b1c8c976001c3f1ff0d
Mr-SpaceMonkey <dario.potenza02@gmail.com>
2017-11-11 10:04:38 +0000

    Add project

---
+++ Skating game/Assets/GameManager.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour {
+
+    public GameObject music;
+
+    private void Awake()
+    {
+        if (music == null)
+        {
+            Instantiate(music);
+        }
+
+    }
+
+    public void Respawn()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        DontDestroyOnLoad(music);
+
+    }
+
+}
