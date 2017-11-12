b892655e7c2a1b490aed4038ff6ef2ede800d1b8
Mr-SpaceMonkey <dario.potenza02@gmail.com>
2017-11-12 00:14:05 +0000

    Added tricks

--- Skating game/Assets/Scripts/PlayerMotor.cs
+++ Skating game/Assets/Scripts/PlayerMotor.cs
@@ -8,21 +8,64 @@ public class PlayerMotor : MonoBehaviour {
     public WheelCollider backLeft;
     public WheelCollider backRight;
 
+    [Header("Movement Feel")]
     public float motorForce;
     public float steerForce;
+    public float jumpHeight;
+    public float shuvitSpeed;
+    public float flipSpeed;
+    public float impossibleSpeed;
+
+    [Header("")]
+    public Transform groundCheck;
+    public LayerMask whatIsGround;
+
+    Animator anim;
+    Rigidbody rb;
+    bool isGrounded;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody>();
+    }
 
     // Update is called once per frame
     void Update ()
     {
+        isGrounded = Physics.Linecast(transform.position, groundCheck.position, whatIsGround);
+        float flip = Input.GetAxis("Flipping");
         float motor = Input.GetAxis("Fire1") * motorForce;
         float turn = Input.GetAxis("Horizontal") * steerForce;
+        float impossible = Input.GetAxis("Vertical");
+        anim.SetFloat("Direction", turn);        backLeft.motorTorque = motor;
 
-        backLeft.motorTorque = motor;
-        backRight.motorTorque = motor;
-        frontLeft.motorTorque = motor;
-        frontRight.motorTorque = motor;
-
-        frontLeft.steerAngle = turn;
-        frontRight.steerAngle = turn;
+        if (isGrounded)
+        {
+            backRight.motorTorque = motor;
+            frontLeft.motorTorque = motor;
+            frontRight.motorTorque = motor;
+            frontLeft.steerAngle = turn;
+            frontRight.steerAngle = turn;
+            if (Input.GetButtonDown("Jump"))
+            {
+                rb.AddForce(Vector3.up * jumpHeight, ForceMode.Impulse);
+            }
+        }
+        else
+        {
+            if(turn != 0)
+            {
+                transform.Rotate(0,shuvitSpeed*turn,0);
+            }
+            if(flip != 0)
+            {
+                transform.Rotate(0, 0, flipSpeed * flip);
+            }
+            if (impossible != 0)
+            {
+                transform.Rotate(impossible * impossibleSpeed, 0, 0);
+            }
+        }
 	}
 }
